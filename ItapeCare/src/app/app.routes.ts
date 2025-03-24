a17e13e6fd60be3cca6cfd09396e9d4ae6a7d8bf
Mateus-Pe <mateus.gpereira019@gmail.com>
2025-03-23 21:19:46 -0300

    Criando o template menu

--- ItapeCare/src/app/app.routes.ts
+++ ItapeCare/src/app/app.routes.ts
@@ -5,5 +5,5 @@ import { LoginComponent } from './login/login.component';
 export const routes: Routes = [
     {path: 'home', component: HomeComponent},
     {path: 'login', component: LoginComponent},
-    { path: '', redirectTo: '/home', pathMatch: 'full' }
+    { path: '', redirectTo: '/login', pathMatch: 'full' }
 ];
--- ItapeCare/src/app/home/home/home.component.ts
+++ ItapeCare/src/app/home/home/home.component.ts
@@ -1,26 +1,15 @@
-import { Component, HostListener } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MenuComponent } from "../../menu/menu/menu.component";
+
 
 @Component({
   selector: 'app-home',
-  imports: [CommonModule],
+  imports: [CommonModule, MenuComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
 
-  isCollapsed: boolean = true;
-  isExpanded: boolean = false;
-
-  @HostListener('mouseenter', ['$event'])
-  onMouseEnter(event: MouseEvent) {
-    this.isCollapsed = false;
-    this.isExpanded = true;
-  }
-
-  @HostListener('mouseleave', ['$event'])
-  onMouseLeave(event: MouseEvent) {
-    this.isCollapsed = true;
-    this.isExpanded = false;
-  }
+  
 }
