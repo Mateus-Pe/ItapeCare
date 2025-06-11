1e23e91829ef2825dd412fe83d3c5e48a4bf480d
Douglas Oliveira <dgzincs@outlook.com.br>
2025-06-11 18:29:06 -0300

    Delete ItapeCare directory
    Removendo a pasta duplicada

--- ItapeCare/src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { HomeComponent } from './home/home/home.component';
-import { LoginComponent } from './login/login.component';
-
-export const routes: Routes = [
-    {path: 'home', component: HomeComponent},
-    {path: 'login', component: LoginComponent},
-    { path: '', redirectTo: '/login', pathMatch: 'full' }
-];
